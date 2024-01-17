d9b0ea1823a17e2d48f4cf6a43418adedeb1151b
José Antonio González Morente <jgonzalezmorente@gmail.com>
2024-01-17 10:51:14 +0100

    Criptoanálisis, resolución del cúbica, proposición básica anillos

--- criptografia/cesar/main.py
+++ criptografia/cesar/main.py
@@ -1,21 +1,24 @@
 
+from collections import Counter
 alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 encoding = { k:v for k, v in zip( alphabet, range(27) ) }
 decoding = { k:v for k, v in zip( range(27), alphabet ) }
+
 def c_key( key: str, letter: str ) -> int:
     assert key in alphabet and letter in alphabet
     return ( encoding[ letter ] + encoding[ key ] ) % 27
 
 #print(encoding)
 
-key = 'W'
-message = 'ESTAESLAPRIMERAVEZQUEVAMOSACIFRARUNMENSAJE'
 
+message = 'ÑWEWUEPKBNÑUKVKWMQKNÑMEIYWYVLBÑWYAERÑBYKMYBNKBVÑ'
 encoded_message = [ encoding[letter] for letter in message ]
-encrypted_message = [ ( x - 4 ) % 27 for x in encoded_message ]
 
+counter = Counter( message )
+for key in counter.most_common(2):    
+     decrypted_message = [ ( x - encoding[ key[0] ] ) % 27 for x in encoded_message ]    
+     print( key[0], ''.join([decoding[x] for x in decrypted_message]) )
 #print( encoded_message )
-print( ''.join([decoding[x] for x in encrypted_message]) )
 
 
 
